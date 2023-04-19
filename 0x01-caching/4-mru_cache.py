5377aeffa1f1af85f2095cfdea20bb1c0bbe99ae
Chijioke Madubuko <105008387+webizone@users.noreply.github.com>
2023-04-19 19:40:08 +0100

    4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""
+Contains the class MRUCache which is an implementation of a
+Most Recently Used (MRU) caching system
+"""
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    Most Recently Used (MRU) caching system implementation.
+    """
+    def __init__(self):
+        """
+        Initialize an LRUCache instance
+        """
+        super().__init__()
+        self.cache_order = []
+
+    def put(self, key, item):
+        """
+        Get an item by using its key and return it. If the key is not in the
+        cache, add it. If the key is in the cache, remove it and add it again.
+
+        Args:
+            key: The key of the item to get.
+            item: The item to add to the cache.
+
+        Returns:
+            The item if it is in the cache, None otherwise.
+        """
+        if key and item:
+            self.cache_data[key] = item
+            if key not in self.cache_order:
+                self.cache_order.append(key)
+            else:
+                self.cache_order.remove(key)
+                self.cache_order.append(key)
+            if len(self.cache_order) > BaseCaching.MAX_ITEMS:
+                popped = self.cache_order.pop(len(self.cache_order) - 2)
+                del self.cache_data[popped]
+                print("DISCARD: {}".format(str(popped)))
+
+    def get(self, key):
+        """
+        Get an item by using its key and return it. If the key is not
+        in the cache, return None.
+
+        Args:
+            key: The key of the item to get.
+        """
+        if key in self.cache_data:
+            self.cache_order.remove(key)
+            self.cache_order.append(key)
+            return self.cache_data[key]
+        return None
